7004a0890da47ea8091e4c33f3b4003948a70ddb
Libo Sun <sunlibo@yahoo.com>
2023-11-04 20:28:44 -0400

    bug fix and update

--- src/static/_libo/js/text_display.js
+++ src/static/_libo/js/text_display.js
@@ -1,23 +1,25 @@
 let search_minimized = false;
 let search_toggle = "";
 let text_select = "";
+let id_pop_row = "";
 
 function populateDropdown() {
   // Array of options to add
   var options = {
-    "Brother Jacob": "Brother Jacob_refine_v1",
-    "Middle March": "Middlemarch_refine_v1",
-    "Adam Bebe": "Adam Bede_refine_v1.1",
-    "Daniel Deronda": "Daniel_Deronda_refine_v1",
-    "Felix Holt": "Felix Holt, the Radical_refine_v1",
-    "Impressions of Theophrastus Such": "Impressions of Theophrastus Such",
-    "Janet's Repentance": "Janet's Repentance",
-    "Mr.Gilfil's Love Story": "Mr.Gilfil's Love Story",
-    Romola: "Romola_refine_v1",
-    "Silas Maner": "Silas Marner",
-    "The Lifted Veil": "The Lifted Veil",
-    "The Mill on the Floss": "The Mill on the Floss",
-    "The Sad Fortunes of the Rev. Amos Barton": "The Sad Fortunes of the Reverend Amos Barton",
+    "Search A Text to Explore": "default_page",
+    "Mr. Gilfil's Love Story (1857)": "Mr.Gilfil's Love Story",
+    "Janet's Repentance (1857)": "Janet's Repentance",
+    "The Sad Fortunes of the Rev. Amos Barton (1857)": "The Sad Fortunes of the Reverend Amos Barton",
+    "Adam Bede (1859)": "Adam Bede_refine_v1.1",
+    "The Lifted Veil (1859)": "The Lifted Veil",
+    "The Mill on the Floss (1860)": "The Mill on the Floss",
+    "Silas Maner (1861)": "Silas Marner",
+    "Romola (1863)": "Romola_refine_v1",
+    "Brother Jacob (1864)": "Brother Jacob_refine_v1",
+    "Felix Holt, the Radical (1866)": "Felix Holt, the Radical_refine_v1",
+    "Middlemarch (1871-72)": "Middlemarch_refine_v1",
+    "Daniel Deronda (1876)": "Daniel_Deronda_refine_v1",
+    "Impressions of Theophrastus Such (1879)": "Impressions of Theophrastus Such",
   };
 
   // Get the select element
@@ -60,38 +62,11 @@ function populateDropdown() {
 
 // Call the function to populate the dropdown
 
-function displayTEIContent_(filename) {
-  fetch("get-xml/" + filename)
-    .then((response) => response.text())
-    .then((data) => {
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString(data, "text/xml");
-
-      // Extract elements
-      const paragraphs = Array.from(xmlDoc.getElementsByTagName("p"));
-      const heads = Array.from(xmlDoc.getElementsByTagName("head"));
-
-      // Convert the XML elements to HTML and append them to the page
-      const displayArea = document.getElementById("xml-display");
-
-      heads.forEach((head) => {
-        const h = document.createElement("h2");
-        h.textContent = head.textContent;
-        displayArea.appendChild(h);
-      });
-
-      paragraphs.forEach((p) => {
-        const para = document.createElement("p");
-        para.textContent = p.textContent;
-        displayArea.appendChild(para);
-      });
-    });
-}
-
 function displayTEIContent(filename) {
   fetch("get-xml/" + filename)
     .then((response) => response.text())
     .then((data) => {
+      hide_search_container();
       const parser = new DOMParser();
       const xmlDoc = parser.parseFromString(data, "text/xml");
       // Serialize XML DOM to string
@@ -103,49 +78,6 @@ function displayTEIContent(filename) {
 
       document.getElementById("xml-display").innerHTML = escapeXml(serializedXml);
     });
-}
-
-// function searchOneAndHighlight(phrase) {
-//   const displayArea = document.getElementById("xml-display");
-//   let innerHTML = displayArea.innerHTML;
-//   const regex = new RegExp(phrase, "gi");
-//   const replacement = `<span class="highlight">${phrase}</span>`;
-//   innerHTML = innerHTML.replace(regex, replacement);
-//   displayArea.innerHTML = innerHTML;
-// }
-
-// function searchWordsAndHighlight(phrase) {
-//   const displayArea = document.getElementById("xml-display");
-//   let innerHTML = displayArea.innerHTML;
-//   // Escape any special characters in the phrase
-//   const escapedPhrase = phrase.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
-//   const regex = new RegExp(`(${escapedPhrase})`, "gi");
-//   const replacement = `<span class="highlight">$1</span>`;
-//   innerHTML = innerHTML.replace(regex, replacement);
-//   displayArea.innerHTML = innerHTML;
-// }
-
-function searchPhraseAndHighlight(phrase) {
-  const displayArea = document.getElementById("xml-display");
-
-  // First, remove existing highlights
-  const highlighted = Array.from(displayArea.querySelectorAll(".highlight"));
-  highlighted.forEach((span) => {
-    const parent = span.parentNode;
-    while (span.firstChild) {
-      parent.insertBefore(span.firstChild, span);
-    }
-    parent.removeChild(span);
-  });
-
-  // Continue with the search and highlighting process
-  let innerHTML = displayArea.innerHTML;
-  // Escape any special characters in the phrase
-  const escapedPhrase = phrase.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
-  const regex = new RegExp(`(${escapedPhrase})`, "gi");
-  const replacement = `<span class="highlight">$1</span>`;
-  innerHTML = innerHTML.replace(regex, replacement);
-  displayArea.innerHTML = innerHTML;
 }
 
 function searchAndHighlight(phrase) {
@@ -175,30 +107,48 @@ function searchAndHighlight(phrase) {
   let counter = 0;
   let innerHTML = displayArea.innerHTML;
 
+  // v1
   // // Escape any special characters in the phrase
   // const escapedPhrase = phrase.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
   // // Adding word boundaries to the regex
   // const regex = new RegExp(`((?:\\w+\\W+){0,3}\\w*)?\\b(${escapedPhrase})\\b(\\w*(?:\\W+\\w+){0,3})?`, "gi");
 
-  // Escape any special characters in the phrase
-  const escapedPhrase = phrase.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
-  // Revised regex to capture strictly two words
+  // v2
+  // const escapedPhrase = phrase.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
   // const regex = new RegExp(`((?:\\w+\\W+){0,2}\\w*)?\\b(${escapedPhrase})\\b(\\w*(?:\\W+\\w+){0,2})?`, "gi");
-  const regex = new RegExp(`((?:\\w+\\W+){0,2}\\w*)?\\b(${escapedPhrase})\\b(\\w*(?:\\W+\\w+){0,2})?`, "gi");
+
+  // v3
+  const escapedPhrase = phrase.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+  const regex = new RegExp(`((?:\\w+\\W+){0,3})?\\b(${escapedPhrase})\\b((?:$|\\W+\\w+){0,3})?`, "gi");
+
+  // // v4
+  // const escapedPhrase = phrase.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+  // console.log(escapedPhrase);
+  // const regex = new RegExp(`((?:^|\\w+\\W+){0,3})?(${escapedPhrase})((?:\\W+\\w+){0,3})?`, "gi");
 
   innerHTML = innerHTML.replace(regex, function (match, p1, p2, p3) {
-    let { p1_revised, p3_revised } = reviseP1P3(p1, p3);
+    // let { p1_revised, p3_revised } = reviseP1P3(p1, p3);
+    let p1_revised = revisePhrase(p1);
+    let p3_revised = revisePhrase(p3);
 
     const id = `match-${counter++}`;
+    const id_pop = `pop-${counter++}`;
     const resultItem = document.createElement("div");
     resultItem.className = "search-result";
-    resultItem.innerHTML = `${p1_revised || ""} <strong>${p2}</strong> ${p3_revised || ""}`;
+    resultItem.innerHTML = `<span  id="${id_pop}">${p1_revised || ""}<strong>${p2}</strong>${p3_revised || ""}</span>`;
     resultItem.addEventListener("click", () => {
       const target = document.getElementById(id);
-
       const targetPosition = target.getBoundingClientRect().top;
       const offset = window.pageYOffset + targetPosition - window.innerHeight / 2;
+      minimize_pop();
       window.scrollTo(0, offset);
+
+      // highlight the target when scroll to it
+      const target_pop_row = document.getElementById(id_pop_row);
+      if (target_pop_row) target_pop_row.classList.remove("text-primary");
+      const target_pop = document.getElementById(id_pop);
+      target_pop.classList.add("text-primary");
+      id_pop_row = id_pop;
 
       // displayArea.scrollTop = targetPosition;
 
@@ -214,7 +164,7 @@ function searchAndHighlight(phrase) {
       // search_results.classList.add("minimized");
     });
     searchResults.appendChild(resultItem);
-    return `${p1 || ""} <span class="highlight" id="${id}">${p2}</span> ${p3 || ""}`;
+    return `${p1 || ""}<span class="highlight" id="${id}">${p2}</span>${p3 || ""}`;
   });
 
   searchContainer.appendChild(searchResults);
@@ -235,12 +185,18 @@ function pop_up_interactive(doc_container, displayed_results, doc_scroll_top) {
   container.style.display = "flex";
   // container.style.alignItems = "center";
   container.style.justifyContent = "space-between";
-  container.className = "position-sticky position-absolute ms-2 mt-1 top-0";
+  container.className = "position-sticky position-absolute mt-1 top-0";
+  if (displayed_results.offsetWidth < 50) {
+    // if the search results are none, set the width to 300px
+    container.style.width = 300 + "px";
+  } else {
+    container.style.width = displayed_results.offsetWidth - 26 + "px";
+  }
 
   // Create a text span or div
   var textDisplay = document.createElement("span");
   textDisplay.className = "start-0 text-primary fs-6";
-  textDisplay.textContent = "Found " + displayed_results.children.length + " results";
+  textDisplay.textContent = displayed_results.children.length + " results";
   container.appendChild(textDisplay); // Append text to the container
 
   const divButtons = document.createElement("div");
@@ -252,18 +208,18 @@ function pop_up_interactive(doc_container, displayed_results, doc_scroll_top) {
   buttonItem.className = "btn btn-outline-primary btn-sm top-0";
   buttonItem.setAttribute("type", "button");
 
-  buttonItem.textContent = "Minimize";
+  buttonItem.textContent = "Min";
   buttonItem.id = "sr_minimize_button";
   buttonItem.addEventListener("click", () => {
     if (search_minimized === false) {
       doc_container.classList.add("minimized");
       displayed_results.classList.add("minimized");
-      buttonItem.textContent = "Maxmize";
+      buttonItem.textContent = "Max";
       search_minimized = true;
     } else {
       doc_container.classList.remove("minimized");
       displayed_results.classList.remove("minimized");
-      buttonItem.textContent = "Minimize";
+      buttonItem.textContent = "Min";
       search_minimized = false;
     }
   });
@@ -276,7 +232,7 @@ function pop_up_interactive(doc_container, displayed_results, doc_scroll_top) {
   buttonScrollTop.className = "btn btn-outline-success btn-sm top-0";
   buttonScrollTop.setAttribute("type", "button");
 
-  buttonScrollTop.textContent = "Scroll Top";
+  buttonScrollTop.textContent = "Top";
   buttonScrollTop.id = "sr_scroll_top_button";
   buttonScrollTop.addEventListener("click", () => {
     doc_scroll_top.scrollIntoView();
@@ -356,31 +312,32 @@ function escapeXml(xmlString) {
   return xmlString.replace(/<q>/gi, "").replace(/<\/q>/gi, "");
 }
 
-function reviseP1P3(p1, p3) {
-  let p1_revised = "";
-  let p3_revised = "";
-  console.log("p1: ", p1);
-  console.log("p3: ", p3);
-  if (p1 !== undefined) {
-    // if (p1.includes("/p")) {
-    //   p1_revised = "";
-    // } else if (p1.includes(">")) {
-    //   let parts = p1.split(">");
-    //   p1_revised = parts[1].trim(); // Assuming you always want the part after ">"
-    // } else {
-    //   p1_revised = p1.trim(); // Just trim the original string if ">" is not present
-    // }
-    p1_revised = p1.replace(/<p\s*$/, "");
-  }
-  if (p3 !== undefined) {
-    // if (p3.includes("<")) {
-    //   let parts = p3.split("<");
-    //   p3_revised = parts[0].trim(); // Assuming you always want the part after ">"
-    // } else {
-    //   p3_revised = p3.trim(); // Just trim the original string if ">" is not present
-    // }
-    p3_revised = p3.replace(/<p\s*$/, "");
+function revisePhrase(phrase) {
+  let p_revised = "";
+
+  if (phrase !== undefined) {
+    p_revised = phrase.replace(/<p\s*$/, "");
   }
 
-  return { p1_revised, p3_revised };
+  return p_revised;
+}
+
+function minimize_pop() {
+  const buttonItem = document.getElementById("sr_minimize_button");
+  const doc_container = document.getElementById("search_container");
+  const displayed_results = document.getElementById("search_results");
+  if (search_minimized === false) {
+    doc_container.classList.add("minimized");
+    displayed_results.classList.add("minimized");
+    buttonItem.textContent = "Max";
+    search_minimized = true;
+  }
+}
+
+function hide_search_container() {
+  const searchContainer = document.getElementById("search_container");
+  if (searchContainer !== null) {
+    // searchContainer.innerHTML = "";
+    searchContainer.style.display = "none";
+  }
 }
